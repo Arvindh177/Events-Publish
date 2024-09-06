0a43978610ca723dd2d91a3335d4d61980d3c0df
Arvindh M <72850966+Arvindh177@users.noreply.github.com>
2024-09-06 21:42:12 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ import PlacePage from './pages/PlacePage'
 import BookingsPage from './pages/BookingsPage'
 import BookingPage from './pages/BookingPage'
 
-axios.defaults.baseURL = 'http://127.0.0.1:4000' 
+axios.defaults.baseURL = 'events-publish-api.vercel.app' 
 axios.defaults.withCredentials = true;
 
 function App() {
