6506c706c664a194ac9e2fc6d4e629a2cc540c11
Arvindh M <72850966+Arvindh177@users.noreply.github.com>
2024-09-07 14:49:06 +0530

    Update PlaceGallery.jsx

--- frontend/src/PlaceGallery.jsx
+++ frontend/src/PlaceGallery.jsx
@@ -34,18 +34,18 @@ export default function PlaceGallery({place}) {
             <div>
                 {place.photos?.[0] && (
                     <div>
-                         <img onClick={()=> setShowAllPhotos(true)} className="aspect-square object-cover cursor-pointer" src={'http://localhost:4000/uploads/'+place.photos[0]}/>
+                         <img onClick={()=> setShowAllPhotos(true)} className="aspect-square object-cover cursor-pointer" src={'https://events-publish.onrender.com/uploads/'+place.photos[0]}/>
                     </div>
                    
                 )}
             </div>
             <div className="grid">
                 {place.photos?.[1] && (
-                    <img onClick={()=> setShowAllPhotos(true)} className="aspect-square object-cover cursor-pointer" src={'http://localhost:4000/uploads/'+place.photos[1]}/>
+                    <img onClick={()=> setShowAllPhotos(true)} className="aspect-square object-cover cursor-pointer" src={'https://events-publish.onrender.com/uploads/'+place.photos[1]}/>
                 )}
                 <div className="overflow-hidden">
                 {place.photos?.[2] && (
-                    <img onClick={()=> setShowAllPhotos(true)} className="aspect-square object-cover cursor-pointer relative top-2" src={'http://localhost:4000/uploads/'+place.photos[2]}/>
+                    <img onClick={()=> setShowAllPhotos(true)} className="aspect-square object-cover cursor-pointer relative top-2" src={'https://events-publish.onrender.com/uploads/'+place.photos[2]}/>
                 )}
                 </div>               
             </div>
